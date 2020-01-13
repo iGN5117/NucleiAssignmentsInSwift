2921f98fdefce98764d7ec70b91ae97fcab287f5
Nilesh Gajwani <nileshgajwani@Nileshs-MacBook-Pro.local>
2020-01-13 15:28:27 +0530

    FirstCommit

---
+++ NucleiAssign2/NucleiAssign2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NucleiAssign2
+//
+//  Created by Nilesh Gajwani on 13/01/20.
+//  Copyright © 2020 Nilesh Gajwani. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
